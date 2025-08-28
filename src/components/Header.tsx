9d3450b78d4f25cddc1634f895d0f190628f3fb7
wpplng <lina.wappling@gmail.com>
2025-08-28 10:41:29 +0200

    added header

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import type { ReactElement } from 'react';
+
+const Header = (): ReactElement => {
+  return (
+    <header className='header'>
+      <h1>Todo</h1>
+    </header>
+  );
+};
+export default Header;
